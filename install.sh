a03dc45384a2ad5c7f3198f9351d04f53a49f594
Raimon Bosch <raimon.bosch@gmail.com>
2018-01-28 23:06:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@
 echo "Building docker sparksql-dashboard container..."
 docker build -t sparksql-dashboard:latest .
 
-echo "Creation of aggregated tables..."
+echo "Creating aggregated tables..."
 docker run -v $(pwd):/dashboard -e DASHBOARD_HOME=/dashboard -w /dashboard sparksql-dashboard:latest /opt/spark/bin/spark-submit src/Dashboard/aggregations.py
